5e341eef98205fe675a7ba3d62f4d392be4e1ccd
tfs-tada <tfs.tada@gmail.com>
2024-07-23 00:01:28 +0900

    add: en mode

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        en: './en.html'
+      }
+    }
+  }
 });
